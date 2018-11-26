d27dc723b51eb69c2c7824bdef8ad00ab2af5e45
goosecoid <adblavier@gmail.com>
2018-11-26 16:45:16 +0100

    temp changes 2

--- huchain-front/src/App.vue
+++ huchain-front/src/App.vue
@@ -2,47 +2,49 @@
   <div id="app">
     <h1>HuChain</h1>
     <vue-form :state="formstate" @submit.prevent="onSubmit">
-    <validate tag="label">
-      <span>First name</span>
-      <input v-model="model.form.firstName" required name="firstName" type="text" minlength="2"/>
-    </validate>
-    <br>
-    <validate tag="label">
-      <span>Last name</span>
-      <input v-model="model.form.lastName" name="lastName" type="text" required minlength="2"/>
-    </validate>
-    <br>
-    <validate tag="label">
-      <span>Email</span>
-      <input v-model="model.form.email" name="email" type="email" required minlength="2"/>
-    </validate>
-    <br>
-    <validate tag="label">
-      <span>Date of birth</span>
-      <input v-model="model.form.dateBirth" name="dateBirth" type="date" required/>
-    </validate>
-    <br>
-    <validate tag="label">
-      <span>Place of birth</span>
-      <input v-model="model.form.placeBirth" name="placeBirth" type="text" required minlength="2"/>
-    </validate>
-    <br>
-    <button v-bind:disabled="this.formstate.$invalid" type="submit">Submit</button>
-  </vue-form>
- </div>
+      <validate tag="label">
+        <span>First name</span>
+        <input v-model="model.form.firstName" required name="firstName" type="text" minlength="2">
+      </validate>
+      <br>
+      <validate tag="label">
+        <span>Last name</span>
+        <input v-model="model.form.lastName" name="lastName" type="text" required minlength="2">
+      </validate>
+      <br>
+      <validate tag="label">
+        <span>Email</span>
+        <input v-model="model.form.email" name="email" type="email" required minlength="2">
+      </validate>
+      <br>
+      <validate tag="label">
+        <span>Date of birth</span>
+        <input v-model="model.form.dateBirth" name="dateBirth" type="date" required>
+      </validate>
+      <br>
+      <validate tag="label">
+        <span>Place of birth</span>
+        <input v-model="model.form.placeBirth" name="placeBirth" type="text" required minlength="2">
+      </validate>
+      <br>
+      <button v-bind:disabled="this.formstate.$invalid" type="submit">Submit</button>
+    </vue-form>
+    <p>{{ model.errormsg }}</p>
+    <p>{{ model.resultmsg }}</p>
+  </div>
 </template>
 
 <script>
-// import hasher
-import { hashString } from "./hasher";
-import { web3 } from "./web3";
+import Web3 from 'web3';
 import data from "../../blockchain/build/contracts/HumainChainIdLogic";
-
-alert(
-    new web3.eth.Contract(
-        data.abi,
-        "0x1eb7d503d5193cbbc1e8280feac65f417765a0cb"
-    )
+import { createHashString } from './hasher';
+//initiate contract
+const web3 = new Web3("http://0.0.0.0:8545");
+window.ethereum.enable();
+web3.eth.defaultAccount = web3.eth.accounts[0];
+const huchainContract = new web3.eth.Contract(
+    data.abi,
+    "0x3828faede1d6901b999026a2dc46875af323a966"
 );
 // start
 export default {
@@ -52,6 +54,8 @@ export default {
         return {
             formstate: {},
             model: {
+                errormsg: "",
+                resultmsg: "",
                 form: {
                     firstName: "",
                     lastName: "",
@@ -65,9 +69,35 @@ export default {
     methods: {
         onSubmit: function() {
             if (this.formstate.$invalid) {
-                return;
+                this.model.errormsg = "Form is invalid";
             } else {
-                alert(hashString(JSON.stringify(this.model.form)));
+                // create unique string to person
+                let formString = `${this.model.form.firstName
+                    .toLowerCase()
+                    .replace(
+                        /\s/g,
+                        ""
+                    )}${this.model.form.lastName
+                    .toLowerCase()
+                    .replace(/\s/g, "")}${String(this.model.form.dateBirth)
+                    .toLowerCase()
+                    .replace(
+                        /\s/g,
+                        ""
+                    )}${this.model.form.placeBirth
+                    .toLowerCase()
+                    .replace(/\s/g, "")}`;
+                // send string to hasher to register in shmart contwact
+                let hash = createHashString(formString);
+                huchainContract.methods.register(hash).call((err, result) => { 
+                    if (result) {
+                        this.model.resultmsg = `Result: ${JSON.stringify(result)}`
+                    } else {
+                        if (err) {
+                            this.model.errormsg = `Error: ${JSON.stringify(err)}`
+                        }        
+                    }
+                })
             }
         }
     }
@@ -81,10 +111,14 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
     margin-top: 60px;
+    text-align: center;
 }
 h1 {
     text-align: center;
     padding: 50px;
+}
+input {
+    margin: 5px;
 }
 </style>
 
--- huchain-front/src/hasher.js
+++ huchain-front/src/hasher.js
@@ -1,10 +1,11 @@
 import * as sjcl from 'sjcl';
 
-export function hashString(input) {
+export function createHashString(input) {
     let arr = (new sjcl.hash.sha256()).update(input).finalize();
     let out = "", i;
     for (i = 0; i < arr.length; i++) {
         out += ((arr[i] | 0) + 0xF00000000000).toString(16).substr(4)
     }
-    return out.substr(0, sjcl.bitArray.bitLength(arr) / 4);
+    let str64 = out.substr(0, sjcl.bitArray.bitLength(arr) / 4);
+    return "0x" + str64.slice(30)
 }
