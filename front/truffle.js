0755968a088171c8993622515e5829dedd5acd4a
goosecoid <adblavier@gmail.com>
2018-11-30 14:50:19 +0100

    (fix)docker-compose|gitignore|truffleconfig

--- front/truffle.js
+++ front/truffle.js
@@ -1,11 +1,10 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
-      network_id: "*", // Match any network id
+      port: "8545",
+      network_id: "*",
       gas: 2000000,
     }
-};
+  }
+}
